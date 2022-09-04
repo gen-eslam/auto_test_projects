39acee6359d0b9c6dabeb74dc9c9eb8d10fb8687
GEN_eslam abdelazize mohamed <78043455+gen-eslam@users.noreply.github.com>
2022-09-04 20:22:27 +0200

    managing framework and get value from file

---
+++ automationframework/src/main/java/DriverStrategyImplementer.java
@@ -0,0 +1,19 @@
+public class DriverStrategyImplementer {
+
+    public static DriverStrategy chooseStrategy(String strategy){
+        switch(strategy){
+            case "Chrome":
+                return new Chrome();
+
+            case "PhantomJs":
+                return new PhantomJs();
+
+            case "Firefox":
+                return new Firefox();
+
+            default:
+                return null;
+        }
+
+    }
+}
---
+++ automationframework/src/main/java/Main.java
@@ -0,0 +1,11 @@
+import org.openqa.selenium.WebDriver;
+
+public class Main {
+    public static void main(String[] args) {
+        FrameworkProperties frameworkProperties = new FrameworkProperties();
+        DriverSingleton.getInstance(frameworkProperties.getProperty("browser"));
+        WebDriver driver = DriverSingleton.getDriver();
+        driver.get("http://automationpractice.com");
+       // driver.quit();
+    }
+}
