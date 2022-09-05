64f96e5fb6d3b265d0e60a79862ff23f261ea413
GEN_eslam abdelazize mohamed <78043455+gen-eslam@users.noreply.github.com>
2022-09-05 04:17:31 +0200

    edit some error

--- automationframework/src/main/java/utils/Constants.java
+++ automationframework/src/main/java/utils/Constants.java
@@ -3,8 +3,8 @@ package utils;
 public class Constants {
     public static final String PROB_FILE_NAME = "framework.properties";
     public static final String FILE_NOT_FOUND_EXCEPTION_MESSAGE ="The Property file has not been found";
-    public static final String CHROME = "Chrome";
-    public static final String FIREFOX ="Firefox";
-    public static final String PHANTOMJS= "PhantomJs";
+    public static final String CHROME = "driver.strategy.Chrome";
+    public static final String FIREFOX ="driver.strategy.Firefox";
+    public static final String PHANTOMJS= "driver.strategy.PhantomJs";
 
 }
