e78d4900fce2a52da4c02211f4331bbd04666155
GEN_eslam abdelazize mohamed <78043455+gen-eslam@users.noreply.github.com>
2022-09-05 02:58:08 +0200

    refactor some file

--- automationframework/src/main/java/DriverStrategy.java
+++
@@ -1,5 +0,0 @@
-import org.openqa.selenium.WebDriver;
-
-public interface DriverStrategy {
-    WebDriver setStrategy();
-}
--- automationframework/src/main/java/Main.java
+++ automationframework/src/main/java/Main.java
@@ -1,4 +1,6 @@
+import driver.DriverSingleton;
 import org.openqa.selenium.WebDriver;
+import utils.FrameworkProperties;
 
 public class Main {
     public static void main(String[] args) {
